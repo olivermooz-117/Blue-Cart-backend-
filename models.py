cc341df9affe8d418dab9c705b204940c3856394
John Muthomi <michenijohn20@gmail.com>
2026-08-26 11:59:19 +0300

    updated models and auth

--- models.py
+++ models.py
@@ -11,6 +11,8 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+
 class SearchHistory(db.Model):
     __tablename__ = "search_history"
 
